0e410e69fe564e1392931f2ed85e6fe23c30e9b4
adithyankumar <adithyankumar.92@gmail.com>
2022-06-19 18:24:35 +0530

    Removed .idea files

--- src/main/kotlin/StringCalculator.kt
+++ src/main/kotlin/StringCalculator.kt
@@ -1,17 +1,20 @@
+class StringCalculator {
+    fun add(input : String) : Int{
+        if (!input.isNullOrEmpty()){
+            val delimiters = ","
+            var sum = 0
+            var splitArray = input.split(delimiters)
+            for (operandStr in splitArray){
+                var operand = operandStr.toInt()
+                sum +=operand
+            }
+            return  sum
+        }
+        return 0
+    }
+}
 fun main(args : Array<String>){
-    println("Sum = ${add("1,2")}")
+    val stringCalculator = StringCalculator()
+    println("Sum = ${stringCalculator.add("1,2")}")
 }
 
-fun add(input : String) : Int{
-    if (!input.isNullOrEmpty()){
-        val delimiters = ","
-        var sum = 0
-        var splitArray = input.split(delimiters)
-        for (operandStr in splitArray){
-            var operand = operandStr.toInt()
-            sum +=operand
-        }
-        return  sum
-    }
-    return 0
-}
--- src/test/kotlin/StringCalculatorTest.kt
+++ src/test/kotlin/StringCalculatorTest.kt
@@ -1,8 +1,28 @@
-import org.junit.Assert.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class StringCalculatorTest {
 
-    @org.junit.Test
-    fun add() {
+    private val stringCalculator = StringCalculator()
+
+    @Test
+    fun `pass empty input string then return 0`() {
+        val inputStr = ""
+        val expected = 0
+        assertEquals(expected , stringCalculator.add(inputStr))
+    }
+
+    @Test
+    fun `pass 1 as input string then return 1`() {
+        val inputStr = "1"
+        val expected = 1
+        assertEquals(expected , stringCalculator.add(inputStr))
+    }
+
+    @Test
+    fun `pass 1,2 as input string then return 3`() {
+        val inputStr = "1,2"
+        val expected = 3
+        assertEquals(expected , stringCalculator.add(inputStr))
     }
 }
